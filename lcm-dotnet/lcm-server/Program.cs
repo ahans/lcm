4433a192b670a98e5532cf1491dd2832bf63b569
ahans <ahans@gmx.net>
2014-11-14 10:41:29 +0100

    print usage and pass address and port to TCPService

--- lcm-dotnet/lcm-server/Program.cs
+++ lcm-dotnet/lcm-server/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+using System;
+using System.Net;
 using LCM.LCM;
 
 namespace LCM.Server
@@ -17,12 +14,28 @@ namespace LCM.Server
             try
             {
                 int port = 7700;
-                if (args.Length > 0)
+                IPAddress address = IPAddress.Any;
+                if ((args.Length > 0 && (args[0] == "-help" || args[0] == "--help" || args[0] == "/h")) || args.Length > 2)
+                {
+                    Console.Error.WriteLine("Usage: lcm-server.exe [ [address to bind to] port ]");
+                    Console.Error.WriteLine("  defaults: any address, port " + port);
+                    Console.Error.WriteLine("  examples:");
+                    Console.Error.WriteLine("  `lcm-server.exe 1234' binds to all interfaces/addresses on port 1234");
+                    Console.Error.WriteLine("  `lcm-server.exe localhost 1234' binds to localhost on port 1234");
+                    return;
+                }
+                else if (args.Length == 1)
                 {
                     port = Int32.Parse(args[0]);
                 }
+                else if (args.Length == 2)
+                {
+                    address = System.Net.Dns.GetHostAddresses(args[0])[0];
+                    port = Int32.Parse(args[1]);
+                }
 
-                new TCPService(port);
+                Console.Out.WriteLine("creating TCPService for " + address.ToString() + ":" + port);
+                new TCPService(address, port);
             }
             catch (System.IO.IOException ex)
             {
